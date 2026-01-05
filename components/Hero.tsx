3ef86495bef565a16bd58f5556075a9140e9fa2c
TharaneeshJ <tharaneeshj@gmail.com>
2026-01-05 10:17:49 +0530

    resume file added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero: React.FC = () => {
   const handleDownloadResume = () => {
     // Creating a temporary link element to trigger the download from the specific local path
     const link = document.createElement('a');
-    link.href = "./Resume.pdf"; // Path to the resume file
+    link.href = './src/resume.jpg'; // Path to the resume file
     link.download = 'TharaneeshJ Resume.pdf';
     document.body.appendChild(link);
     link.click();
